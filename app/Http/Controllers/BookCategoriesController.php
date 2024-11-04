8fef86164150d384fc447455b7d3fb8cf79b636b
diovalda <ferrenxx@gmail.com>
2024-11-04 07:18:34 +0700

    first commit

---
+++ app/Http/Controllers/BookCategoriesController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BookCategories;
+use Illuminate\Http\Request;
+
+class BookCategoriesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(BookCategories $bookCategories)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(BookCategories $bookCategories)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, BookCategories $bookCategories)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(BookCategories $bookCategories)
+    {
+        //
+    }
+}
